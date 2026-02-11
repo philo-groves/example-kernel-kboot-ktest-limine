61c64d31c772d2906f8ae160254c9fbed46e004f
philo-groves <philogroves@gmail.com>
2026-02-11 18:08:04 -0500

    Update formatting

--- crates/basic-crate/src/lib.rs
+++ crates/basic-crate/src/lib.rs
@@ -1,8 +1,7 @@
 #![no_std]
 #![cfg_attr(test, no_main)]
-
-#![cfg_attr(test, feature(custom_test_frameworks))]          // test setup: enable custom test frameworks
-#![cfg_attr(test, test_runner(kunit::runner))]               // test setup: use the custom test runner only in test mode
+#![cfg_attr(test, feature(custom_test_frameworks))] // test setup: enable custom test frameworks
+#![cfg_attr(test, test_runner(kunit::runner))] // test setup: use the custom test runner only in test mode
 #![cfg_attr(test, reexport_test_harness_main = "test_main")] // test setup: rename the test harness entry point
 
 #[cfg(test)]
